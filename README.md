c12edf79a766b5aebf4daa214e2ac2ff4079e271
07martin12 <alexmartin9c@gmail.com>
2024-09-15 04:30:27 -0300

    Se agrego el diagrama de relación, el readme.md y una correción de la hoja sql de la base de datos

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# web-2-tp-1
+# Base de datos para un bloc de notas
+
+## Desarrolladores: 
+* Nombre: Joel Fuentes 
+    * Email: fuenteslautarojoe@gmail.com
+* Nombre: Martín Lorenzi
+    * Email: alexmartir9c@gmail.com 
+
+**El siguiente proyecto tiene como finalidad realizar una base de datos destinada a una aplicación de notas. En esta aplicación el usuario contará con las siguientes funcionalidades:**
+
+1. **El usuario puede registrarse en el sitio y obtener una libreta de notas.**
+2. **Con esta libreta el usuario podrá crear, solicitar, modificar, o eliminar sus notas.**
+3. **Las notas del usuario son identificadas por fechas y estás permiten su futura consulta o modificación.**
+
+![diagrama de relacion](diagramaDeRelacion.png)
+
+**En el siguiente “diagrama de relación”, se puede observar cómo las tres tablas están relacionadas mutuamente formando una estructura de datos jerárquica que facilita la modificación y consulta de los datos almacenados. En esta estructura jerárquica, cada tabla utiliza una “clave primaria” en uno de sus campos para identificar de manera única cada registro, evitando duplicados y asegurando la integridad de sus datos. Esto permite que otra tabla utilice una “clave foránea” en uno de sus campos para realizar una referencia a la clave primaria de la tabla con la que se quiera relacionar, asegurando de esta forma que los registros de ambos campos sean idénticos y mantengan una coherencia entre tablas.**
+
+**Por lo tanto para el desarrollo de esta propuesta se considera que:**
+
+1. **Un usuario posee una libreta con notas.**
+2. **Las notas del usuario se agrupan por fechas (por lo tanto una misma fecha puede contener varias notas).**
+3. **Las notas de cada fecha se componen de un título y de un texto.**
+
+**Estas relaciones y propiedades quedan expuestas en el “diagrama derelación” donde la tabla “usuarios” es la encargada de almacenar los datos de cada usuario del sitio y conectarse mediante el campo “idUsuario” con la tabla “libretas”, encargada de almacenar el número total de notas del usuario y las fechas de su creación. Por otro lado, la tabla “libretas” mediante el campo “idFecha” se relaciona con la tabla “notas” encargada de almacenar el contenido de cada nota y su título correspondiente.** 
+
+
+
---
+++ web2-tp-1.sql
@@ -0,0 +1,208 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-09-2024 a las 09:26:21
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `web2-tp-1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libretas`
+--
+
+CREATE TABLE `libretas` (
+  `idFecha` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `fechaNota` date NOT NULL,
+  `cantNotas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `libretas`:
+--   `idUsuario`
+--       `usuarios` -> `idUsuario`
+--
+
+--
+-- Volcado de datos para la tabla `libretas`
+--
+
+INSERT INTO `libretas` (`idFecha`, `idUsuario`, `fechaNota`, `cantNotas`) VALUES
+(1, 1, '2024-09-12', 2),
+(2, 1, '2024-09-13', 1),
+(3, 2, '2024-09-12', 3),
+(4, 2, '2024-09-14', 2),
+(5, 3, '2024-09-13', 3),
+(6, 3, '2024-09-14', 2),
+(7, 3, '2024-09-15', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notas`
+--
+
+CREATE TABLE `notas` (
+  `idNota` int(11) NOT NULL,
+  `idFecha` int(11) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `nota` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `notas`:
+--   `idFecha`
+--       `libretas` -> `idFecha`
+--
+
+--
+-- Volcado de datos para la tabla `notas`
+--
+
+INSERT INTO `notas` (`idNota`, `idFecha`, `titulo`, `nota`) VALUES
+(1, 1, 'tareas de programación', 'lorem ipsum…'),
+(2, 1, 'fechas de parciales', 'lorem ipsum…'),
+(3, 2, 'compras del día', 'lorem ipsum…'),
+(4, 3, 'comprar comida para el gato', 'lorem ipsum…'),
+(5, 3, 'sacar a pasear al perro', 'lorem ipsum…'),
+(6, 3, 'cortar el pasto', 'lorem ipsum…'),
+(7, 4, 'rutina de entrenamiento', 'lorem ipsum…'),
+(8, 4, 'recetas de cocina', 'lorem ipsum…'),
+(9, 5, 'líneas de colectivo', 'lorem ipsum…'),
+(10, 5, 'precios de computadoras', 'lorem ipsum…'),
+(11, 5, 'notas rápidas', 'lorem ipsum…'),
+(12, 6, 'tareas de ingles', 'lorem ipsum…'),
+(13, 6, 'tp-1-web-2 requerimientos', 'lorem ipsum…'),
+(14, 7, 'gastos semanales', 'lorem ipsum…');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idUsuario` int(11) NOT NULL,
+  `nombreUsuario` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `usuarios`:
+--
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUsuario`, `nombreUsuario`, `email`, `password`) VALUES
+(1, 'joel.l.fuentes', 'fuenteslautarojoe@gmail.com', '*****'),
+(2, '07Martin12', 'alexmartin9c@gmail.com', '****'),
+(3, 'alexander.24', 'alex2024@gmail.com', '****');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `libretas`
+--
+ALTER TABLE `libretas`
+  ADD PRIMARY KEY (`idFecha`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indices de la tabla `notas`
+--
+ALTER TABLE `notas`
+  ADD PRIMARY KEY (`idNota`),
+  ADD KEY `idFecha` (`idFecha`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `libretas`
+--
+ALTER TABLE `libretas`
+  MODIFY `idFecha` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `notas`
+--
+ALTER TABLE `notas`
+  MODIFY `idNota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `libretas`
+--
+ALTER TABLE `libretas`
+  ADD CONSTRAINT `libretas_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`);
+
+--
+-- Filtros para la tabla `notas`
+--
+ALTER TABLE `notas`
+  ADD CONSTRAINT `notas_ibfk_1` FOREIGN KEY (`idFecha`) REFERENCES `libretas` (`idFecha`);
+
+
+--
+-- Metadatos
+--
+USE `phpmyadmin`;
+
+--
+-- Metadatos para la tabla libretas
+--
+
+--
+-- Metadatos para la tabla notas
+--
+
+--
+-- Metadatos para la tabla usuarios
+--
+
+--
+-- Metadatos para la base de datos web2-tp-1
+--
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
