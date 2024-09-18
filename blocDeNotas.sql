9d083a0a8d5dd33b92802528a67e0a1a69cb7de4
07martin12 <alexmartin9c@gmail.com>
2024-09-18 19:22:50 -0300

    finalizo la revision del codigo sql, el diagrama de relacion, y el readme

--- blocDeNotas.sql
+++ blocDeNotas.sql
@@ -1,9 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-09-2024 a las 09:26:21
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -13,50 +17,80 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `blocDeNotas`
+
+CREATE DATABASE `blocDeNotas`;
+--
+
+-- --------------------------------------------------------
+
+-- Estructura de tabla para la tabla `libretas`
+
+CREATE TABLE `blocDeNotas`.`libretas` (
+  `idFecha` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `fechaNota` date NOT NULL,
+  `cantNotas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `libretas`:
+--   `idUsuario`
+--       `usuarios` -> `idUsuario`
+--
+
+--
+-- Volcado de datos para la tabla `libretas`
+--
+
+INSERT INTO `blocDeNotas`.`libretas` (`idFecha`, `idUsuario`, `fechaNota`, `cantNotas`) VALUES
+(1, 1, '2024-09-12', 2),
+(2, 1, '2024-09-13', 1),
+(3, 2, '2024-09-12', 3),
+(4, 2, '2024-09-14', 2),
+(5, 3, '2024-09-13', 3),
+(6, 3, '2024-09-14', 2),
+(7, 3, '2024-09-15', 1);
+
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `notas`
 --
 
-CREATE TABLE `notas` (
-  `id_nota` int(11) NOT NULL,
-  `id_fecha_nota` int(11) NOT NULL,
-  `titulo` varchar(50) DEFAULT NULL,
-  `nota` varchar(2000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+CREATE TABLE `blocDeNotas`.`notas` (
+  `idNota` int(11) NOT NULL,
+  `idFecha` int(11) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `nota` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `notas`:
+--   `idFecha`
+--       `libretas` -> `idFecha`
+--
 
 --
 -- Volcado de datos para la tabla `notas`
 --
 
-INSERT INTO `notas` (`id_nota`, `id_fecha_nota`, `titulo`, `nota`) VALUES
-(1, 1, 'Nota de Hoy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis efficitur lectus arcu, elementum fermentum est sagittis ac. Integer rutrum odio vel sem cursus suscipit. Integer pharetra nec sem vel rhoncus. Fusce in tincidunt lorem, sit amet luctus sem. Duis elementum augue at est maximus gravida. Nulla eget tincidunt ipsum. Aliquam vulputate elit ut faucibus posuere.'),
-(2, 1, 'Apuntes sobre Web', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis efficitur lectus arcu, elementum fermentum est sagittis ac. Integer rutrum odio vel sem cursus suscipit. Integer pharetra nec sem vel rhoncus. Fusce in tincidunt lorem, sit amet luctus sem. Duis elementum augue at est maximus gravida. Nulla eget tincidunt ipsum. Aliquam vulputate elit ut faucibus posuere. '),
-(3, 2, 'Nota día Miércoles', 'Cras eleifend lacinia mauris vitae egestas. In elementum ullamcorper mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis cursus odio ac varius mollis. In ut ultrices dolor, venenatis ultrices neque. Cras hendrerit elit nibh, sed efficitur libero posuere faucibus. Integer ut ex pretium, laoreet eros in, facilisis est. Cras viverra volutpat faucibus. Vestibulum venenatis mattis blandit. Duis dictum pellentesque velit a fringilla. Suspendisse hendrerit posuere nibh.'),
-(4, 3, 'Tareas de Programaci', 'Cras eleifend lacinia mauris vitae egestas. In elementum ullamcorper mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis cursus odio ac varius mollis. In ut ultrices dolor, venenatis ultrices neque. Cras hendrerit elit nibh, sed efficitur libero posuere faucibus. Integer ut ex pretium, laoreet eros in, facilisis est. Cras viverra volutpat faucibus. Vestibulum venenatis mattis blandit. Duis dictum pellentesque velit a fringilla. Suspendisse hendrerit posuere nibh.');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `notasPorFecha`
---
-
-CREATE TABLE `notasPorFecha` (
-  `id_fecha_nota` int(11) NOT NULL,
-  `id_usuario` int(11) DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `cant_notas` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Volcado de datos para la tabla `notasPorFecha`
---
-
-INSERT INTO `notasPorFecha` (`id_usuario`, `id_fecha_nota`, `fecha`, `cant_notas`) VALUES
-(1, 1, '2024-09-14', 2),
-(2, 1, '2024-09-11', 1),
-(3, 2, '2024-09-14', 1);
+INSERT INTO `blocDeNotas`.`notas` (`idNota`, `idFecha`, `titulo`, `nota`) VALUES
+(1, 1, 'tareas de programación', 'lorem ipsum…'),
+(2, 1, 'fechas de parciales', 'lorem ipsum…'),
+(3, 2, 'compras del día', 'lorem ipsum…'),
+(4, 3, 'comprar comida para el gato', 'lorem ipsum…'),
+(5, 3, 'sacar a pasear al perro', 'lorem ipsum…'),
+(6, 3, 'cortar el pasto', 'lorem ipsum…'),
+(7, 4, 'rutina de entrenamiento', 'lorem ipsum…'),
+(8, 4, 'recetas de cocina', 'lorem ipsum…'),
+(9, 5, 'líneas de colectivo', 'lorem ipsum…'),
+(10, 5, 'precios de computadoras', 'lorem ipsum…'),
+(11, 5, 'notas rápidas', 'lorem ipsum…'),
+(12, 6, 'tareas de ingles', 'lorem ipsum…'),
+(13, 6, 'tp-1-web-2 requerimientos', 'lorem ipsum…'),
+(14, 7, 'gastos semanales', 'lorem ipsum…');
 
 -- --------------------------------------------------------
 
@@ -64,85 +98,109 @@ INSERT INTO `notasPorFecha` (`id_usuario`, `id_fecha_nota`, `fecha`, `cant_notas
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `id_usuario` int(11) NOT NULL,
-  `nombre_usuario` varchar(50) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+CREATE TABLE `blocDeNotas`.`usuarios` (
+  `idUsuario` int(11) NOT NULL,
+  `nombreUsuario` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `usuarios`:
+--
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `email`, `password`) VALUES
-(1, 'joel.l.fuentes', 'fuenteslautarojoe@gmail.com', '123contraseña'),
-(2, 'martin24', 'martin24@gmail.com', '#64#contra');
+INSERT INTO `blocDeNotas`.`usuarios` (`idUsuario`, `nombreUsuario`, `email`, `password`) VALUES
+(1, 'joel.l.fuentes', 'fuenteslautarojoe@gmail.com', '*****'),
+(2, '07Martin12', 'alexmartin9c@gmail.com', '****'),
+(3, 'alexander.24', 'alex2024@gmail.com', '****');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `notas`
+-- Indices de la tabla `libretas`
 --
-ALTER TABLE `notas`
-  ADD PRIMARY KEY (`id_nota`),
-  ADD KEY `id_nota` (`id_fecha_nota`) USING BTREE;
+ALTER TABLE `blocDeNotas`.`libretas`
+  ADD PRIMARY KEY (`idFecha`),
+  ADD KEY `idUsuario` (`idUsuario`);
 
 --
--- Indices de la tabla `notasPorFecha`
+-- Indices de la tabla `notas`
 --
-ALTER TABLE `notasPorFecha`
-  ADD PRIMARY KEY (`id_fecha_nota`),
-  ADD UNIQUE KEY `unique_usuario_fecha` (`id_usuario`,`fecha`),
-  ADD KEY `id_usuario` (`id_usuario`);
+ALTER TABLE `blocDeNotas`.`notas`
+  ADD PRIMARY KEY (`idNota`),
+  ADD KEY `idFecha` (`idFecha`);
 
 --
 -- Indices de la tabla `usuarios`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usuario`),
-  ADD UNIQUE KEY `unique_nombre_usuario` (`nombre_usuario`),
-  ADD UNIQUE KEY `unique_email` (`email`);
+ALTER TABLE `blocDeNotas`.`usuarios`
+  ADD PRIMARY KEY (`idUsuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `notas`
+-- AUTO_INCREMENT de la tabla `libretas`
 --
-ALTER TABLE `notas`
-  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `blocDeNotas`.`libretas`
+  MODIFY `idFecha` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT de la tabla `notasPorFecha`
+-- AUTO_INCREMENT de la tabla `notas`
 --
-ALTER TABLE `notasPorFecha`
-  MODIFY `id_fecha_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `blocDeNotas`.`notas`
+  MODIFY `idNota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
-ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+ALTER TABLE `blocDeNotas`.`usuarios`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `notas`
+-- Filtros para la tabla `libretas`
 --
-ALTER TABLE `notas`
-  ADD CONSTRAINT `notas_ibfk_1` FOREIGN KEY (`id_fecha_nota`) REFERENCES `notasPorFecha` (`id_fecha_nota`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `blocDeNotas`.`libretas`
+  ADD CONSTRAINT `libretas_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`);
 
 --
--- Filtros para la tabla `notasPorFecha`
+-- Filtros para la tabla `notas`
 --
-ALTER TABLE `notasPorFecha`
-  ADD CONSTRAINT `notasPorFecha_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `blocDeNotas`.`notas`
+  ADD CONSTRAINT `notas_ibfk_1` FOREIGN KEY (`idFecha`) REFERENCES `libretas` (`idFecha`);
+
+
+--
+-- Metadatos
+--
+USE `phpmyadmin`;
+
+--
+-- Metadatos para la tabla libretas
+--
+
+--
+-- Metadatos para la tabla notas
+--
+
+--
+-- Metadatos para la tabla usuarios
+--
+
+--
+-- Metadatos para la base de datos web2-tp-1
+--
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
